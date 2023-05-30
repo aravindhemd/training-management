21c86be9b7dbce89e8f4bce5a802f155796e215c
Aravind <ajey33@gmail.com>
2023-05-30 12:05:53 +0530

    skills CRUD

--- training-management/src/components/Skill.js
+++ training-management/src/components/Skill.js
@@ -1,55 +1,217 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Box, Button, Typography } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+import Modal from "@mui/material/Modal";
+import Snackbar from "@mui/material/Snackbar";
+import TextField from "@mui/material/TextField";
 import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import { useEffect, useState } from "react";
-import getSkillsAPI from '../utils/api'
-import axiosInstance from "../axiosInstance";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import getSkillsAPI, { addSkillAPI, deleteSkillAPI, editSkillAPI } from "../utils/api";
 import CheckAuth from "../utils/checkAuth";
 
-const columns = [
-  { field: "skillId", headerName: "ID", width: 70 },
-  { field: "skillName", headerName: "Skill name", width: 130 },
-  { field: "category", headerName: "Category", width: 130 },
-  {
-    field: "actions", headerName: "Action", type: 'actions',
-    width: 100,
-    getActions: () => [
-      <GridActionsCellItem icon={<EditIcon />} label="Edit" />,
-      <GridActionsCellItem icon={<DeleteIcon />} label="Delete" />,
-    ],
-  }
-];
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function Skills() {
   let [skillsData, setSkillsData] = useState("");
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => { setOpen(false); reset(); setEditFlag(false)};
+  const [respFlag, setRespFlag] = useState(false);
+  const [resMsg, setResMsg] = useState(false);
+  const [skillData, setSkillData] = useState({});
+  const [editFlag, setEditFlag] = useState(false);
+
+  const columns = [
+    { field: "skillId", headerName: "ID", width: 70 },
+    { field: "skillName", headerName: "Skill name", width: 130 },
+    { field: "category", headerName: "Category", width: 130 },
+    {
+      field: "edit",
+      headerName: "Edit",
+      width: 130,
+      type: "actions",
+      getActions: (params) => [
+        <GridActionsCellItem
+          icon={<EditIcon />}
+          label="Edit"
+          onClick={() => editSkill(params.row)}
+        />,
+      ],
+    },
+    {
+      field: "delete",
+      headerName: "Delete",
+      width: 130,
+      type: "actions",
+      getActions: (params) => [
+        <GridActionsCellItem
+          icon={<DeleteIcon />}
+          label="Delete"
+          onClick={() => deleteSkill(params.row.skillId)}
+        />,
+      ],
+    },
+  ];
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset 
+  } = useForm();
 
   async function getSkills() {
     let data = await getSkillsAPI();
     setSkillsData(data);
   }
-  CheckAuth()
+
+  async function addSkill(payload) {
+    let data = await addSkillAPI(payload);
+    setRespFlag(true);
+    setResMsg(data.message);
+    setOpen(false);
+  }
+
+  async function editSkillData(payload){
+    let data = await editSkillAPI(payload);
+    setRespFlag(true);
+    setResMsg(data.message);
+    setOpen(false);
+  }
+
+  async function deleteSkill(skillId) {
+    let data = await deleteSkillAPI(skillId);
+    setRespFlag(true);
+    setResMsg("Skill deleted successfully");
+  }
+
+  function editSkill(payload){
+    handleOpen()
+    setSkillData({
+      "name":payload.skillName,
+      "category":payload.category
+    })
+    setEditFlag(true);
+  }
+
+  CheckAuth();
+
+  const getData = (data) => {
+    console.log(data)
+    if(!editFlag)
+      addSkill(data);
+    else
+      editSkillData(data);
+  };
 
   useEffect(() => {
     getSkills();
   }, []);
 
+  const handleAlertClose = (event, reason) => {
+    setRespFlag(false);
+  };
+
   return (
-    <div style={{ height: 400, width: "60%", margin: "auto", marginTop: 20 }}>
-      {skillsData &&
-        <DataGrid
-          getRowId={(row) => row.skillId}
-          rows={skillsData.skillList}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
-          pageSizeOptions={[5, 10]}
-          checkboxSelection
-        />
-      }
-    </div>
+    <Box>
+      <Snackbar
+        open={respFlag}
+        autoHideDuration={3000}
+        onClose={handleAlertClose}
+      >
+        <Alert
+          onClose={handleAlertClose}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          {resMsg}
+        </Alert>
+      </Snackbar>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form onSubmit={handleSubmit(getData)}>
+            <div
+              className="form-control"
+              style={{ display: "flex", flexDirection: "column" }}
+            >
+              <TextField
+                id="outlined-disabled"
+                label="Skill Name"
+                defaultValue= {skillData.name ? skillData.name : ""}
+                {...register("skillName")}
+                sx={{ margin: "20px" }}
+              />
+              <TextField
+                id="outlined-disabled"
+                label="Category"
+                defaultValue={skillData.category ? skillData.category : ""}
+                {...register("category")}
+                sx={{ margin: "20px" }}
+              />
+              <Button variant="contained" sx={{ margin: "20px" }} type="submit">
+                Submit
+              </Button>
+            </div>
+          </form>
+        </Box>
+      </Modal>
+
+      <Box
+        display={"flex"}
+        justifyContent={"flex-end"}
+        flexDirection={{ lg: "row", md: "row", sm: "column", xs: "column" }}
+      >
+        <Button
+          variant="contained"
+          sx={{ margin: "20px" }}
+          onClick={handleOpen}
+        >
+          Add Skill
+        </Button>
+      </Box>
+      {skillsData && (
+        <Box sx={{ maxWidth: "60%", marginInline: "auto" }}>
+          <Typography>Skills List</Typography>
+          <DataGrid
+            getRowId={(row) => row.skillId}
+            rows={skillsData.skillList}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+            sx={{ justifyContent: "center" }}
+          />
+        </Box>
+      )}
+    </Box>
+    // <div style={{ height: 400, width: "60%", margin: "auto", marginTop: 20 }}>
+
+    // </div>
   );
 }
 export default Skills;
--- training-management/src/utils/api.js
+++ training-management/src/utils/api.js
@@ -1,10 +1,23 @@
-import axiosInstance  from "../axiosInstance";
+import axiosInstance from "../axiosInstance";
 
-const apiUrl = "/skills"
+const apiUrl = "/skills";
 
-export default function getSkillsAPI(){
-    const  data = axiosInstance
-    .get("/skills/")
-    .then((res) => res.data);
+export default function getSkillsAPI() {
+  const data = axiosInstance.get("/skills/").then((res) => res.data);
+  return data;
+}
+
+export function addSkillAPI(payload) {
+  const data = axiosInstance.post("/skills/", payload).then((res) => res.data);
+  return data;
+}
+
+export function deleteSkillAPI(skillId){
+    const data = axiosInstance.post("/skills/delete/"+skillId).then((res) => res.data);
+    return data;
+}
+
+export function editSkillAPI(payload){
+    const data = axiosInstance.post(apiUrl +"/edit",payload).then((res) => res.data)
     return data;
 }
